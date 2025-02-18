fea48e05bcc8147b1733ea6be46bd98e71aa01ce
lithika104 <lithikasivamani@gmail.com>
2025-02-18 11:54:57 +0530

    Add files via upload

---
+++ SortArray.java
@@ -0,0 +1,23 @@
+public class SortArray {
+    public static void main(String[] args) {
+       
+        int[] numbers = {10, 5};
+
+      
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+
+        
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
